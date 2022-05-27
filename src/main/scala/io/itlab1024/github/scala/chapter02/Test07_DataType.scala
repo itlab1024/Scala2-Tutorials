564bfaa04f66353b973aaea0091c425738e8f9db
shizc4 <shizc4@lenovo.com>
2022-05-27 17:22:53 +0800

    dataType

--- src/main/scala/io/itlab1024/github/scala/chapter02/Test07_DataType.scala
+++ src/main/scala/io/itlab1024/github/scala/chapter02/Test07_DataType.scala
@@ -58,12 +58,14 @@ object Test07_DataType {
     val o: Unit = ()
     // Null
     val p: Null = null
+
     // Nothing,比如下面方法可以返回Nothing，但是实际并不这么做，因为他是所有类型的子类
-    def  nothingTest() : Nothing = {
+    def nothingTest(): Nothing = {
       throw RuntimeException // 必须抛出异常，否则提示返回类型应该是Void
     }
+
     //实际我们可能会这样做
-    def nothingTest(i: Int) : Int = {
+    def nothingTest(i: Int): Int = {
       if (i == 0)
         throw RuntimeException // 必须抛出异常，否则提示返回类型应该是Void
       else
